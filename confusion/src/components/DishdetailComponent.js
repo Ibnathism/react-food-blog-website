c51e06e9b98934c47b25274ff5d19299db2aac3c
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2020-05-03 16:26:27 +0600

    add comments

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,10 +2,66 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-    render() {
+    
+
+    renderDish(dish) {
+        if (dish!=null) {
+            return(
+                <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                    
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-5 m-1">
+                    {this.renderComments(dish.comments)}
+                </div>
+                </div>
+            )
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(comments){
+        const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        const commentList = comments.map(i => {
+            var date = new Date(i.date);
+            let formatted_date = months[date.getMonth()] +" "+ date.getDate()+","+  date.getFullYear();
+            return(
+
+                <div key={i.id}>
+                    <ul className="list-unstyled">
+                    <li>{i.comment}</li>
+                    </ul>
+                    <ul className="list-unstyled">
+                    <li>-- {i.author} , {formatted_date}</li>
+                    </ul>
+                </div>
+            )
+        })
         return(
-            <div className="row">
-                {this.renderDish(this.state.selectedDish)}
+            <div> 
+                <h4>Comments</h4>
+                {commentList}
+            </div>
+            
+        )
+    }
+    render() {
+        const dish = this.props.selectedDish;
+        return(        
+            <div>
+                {this.renderDish(dish)}
             </div>
         );
     }
